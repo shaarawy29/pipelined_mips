8a2cc4c61613468b78a6cc2c8608dad1fb12c472
shaarawy29 <ashaarawy19@gmail.com>
2022-12-17 13:42:10 +0200

    enhance reg_file, edit the test bench

--- mini_mips_32.srcs/sources_1/new/reg_file.vhd
+++ mini_mips_32.srcs/sources_1/new/reg_file.vhd
@@ -38,16 +38,16 @@ use IEEE.std_logic_textio.all;
 --use UNISIM.VComponents.all;
 
 entity reg_file is
-    Port ( clk : in STD_LOGIC;
-           WE : in STD_LOGIC;
-           RA1 : in STD_LOGIC_VECTOR (4 downto 0);
-           RA2 : in STD_LOGIC_VECTOR (4 downto 0);
-           RA3 : in STD_LOGIC_VECTOR (4 downto 0);
-           WA : in STD_LOGIC_VECTOR (4 downto 0);
-           WD : in STD_LOGIC_VECTOR (31 downto 0);
-           RD1 : out STD_LOGIC_VECTOR (31 downto 0);
-           RD2 : out STD_LOGIC_VECTOR (31 downto 0);
-           RD3 : out STD_LOGIC_VECTOR (31 downto 0));
+    Port ( nclk : in STD_LOGIC; -- negative clk, it is triggered at the negative edge of the clk
+           WE : in STD_LOGIC; -- write enable pin, to enable write in the WA register 
+           RA1 : in STD_LOGIC_VECTOR (4 downto 0); -- address of the first operand, source register 
+           RA2 : in STD_LOGIC_VECTOR (4 downto 0); -- address of the second operand, target register 
+           RA3 : in STD_LOGIC_VECTOR (4 downto 0); -- address of the third register, destination register, it is only used in case of weight Inst, to make W3 = Rd 
+           WA : in STD_LOGIC_VECTOR (4 downto 0); -- Write adress, the address of the target or the distination depends on the opcode
+           WD : in STD_LOGIC_VECTOR (31 downto 0); -- Write date, the date to be writen in the WA address
+           RD1 : out STD_LOGIC_VECTOR (31 downto 0); -- Read data 1, the date stored in RA1 source register 
+           RD2 : out STD_LOGIC_VECTOR (31 downto 0); -- Read data 2, the data stored in the RA2 target register 
+           RD3 : out STD_LOGIC_VECTOR (31 downto 0)); -- Read data 3, the data stored in the RA3, destination reg, it is only used in case of weight Inst
 end reg_file;
 
 architecture Behavioral of reg_file is
@@ -57,14 +57,23 @@ shared variable reg_file1 : reg_file  := (others => X"00000000");
 
 begin
 
-process(clk) 
+process(nclk) 
     begin 
-       if ( clk'event and clk = '1') then
+       if ( nclk'event and nclk = '1') then
             RD1 <= reg_file1(to_integer(unsigned(RA1)));
             RD2 <= reg_file1(to_integer(unsigned(RA2)));
             RD3 <= reg_file1(to_integer(unsigned(RA3)));
             if(WE = '1') then
                reg_file1(to_integer(unsigned(WA))) := WD; 
+            end if;
+            if (RA1 = "00000") then 
+                RD1 <= X"00000000";
+            end if;
+            if (RA2 = "00000") then 
+                RD2 <= X"00000000";
+            end if;
+            if (RA3 = "00000") then 
+                RD3 <= X"00000000";
             end if;
        end if; 
     end process;
