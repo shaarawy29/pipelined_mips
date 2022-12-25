bc2b00cd8cd69df6d07ca93896d1cd1b8a80eaf0
shaarawy29 <ashaarawy19@gmail.com>
2022-12-25 16:53:35 +0200

    add ex stage test bench

--- mini_mips_32.srcs/sim_1/new/execute_stage_tb.vhd
+++ mini_mips_32.srcs/sim_1/new/execute_stage_tb.vhd
@@ -37,7 +37,88 @@ end execute_stage_tb;
 
 architecture Behavioral of execute_stage_tb is
     
+    component execute_stage is 
+        Port ( clk : in STD_LOGIC;
+               RegWriteE : in STD_LOGIC;
+               MemtoRegE : in STD_LOGIC;
+               MemWriteE : in STD_LOGIC;
+               ALUControlE : in STD_LOGIC_VECTOR (2 downto 0);
+               ALUSrcE : in STD_LOGIC;
+               RegDstE : in STD_LOGIC;
+               RD1E : in STD_LOGIC_VECTOR (31 downto 0);
+               RD2E : in STD_LOGIC_VECTOR (31 downto 0);
+               RD3E : in STD_LOGIC_VECTOR (31 downto 0);
+               RsE : in STD_LOGIC_VECTOR (4 downto 0);
+               RtE : in STD_LOGIC_VECTOR (4 downto 0);
+               RdE : in STD_LOGIC_VECTOR (4 downto 0);
+               SignImmE : in STD_LOGIC_VECTOR (31 downto 0);
+               CLR : in STD_LOGIC;
+               --nEN : in STD_LOGIC;
+               forwardAE : in std_logic_vector(1 downto 0);
+               forwardBE : in std_logic_vector(1 downto 0);
+               forwardCE : in std_logic_vector(1 downto 0);
+               ResultW : in std_logic_vector(31 downto 0);
+               --WriteRegE : inout std_logic_vector(4 downto 0);
+               RegWriteM : out STD_LOGIC;
+               MemtoRegM : out STD_LOGIC;
+               MemWriteM : out STD_LOGIC;
+               ALUOutM : inout STD_LOGIC_VECTOR (31 downto 0);
+               WriteDataM : out STD_LOGIC_VECTOR (31 downto 0);
+               WriteRegM : out STD_LOGIC_VECTOR (4 downto 0));
+    end component;
+    
+    -- execute state signals 
+    signal clk : STD_LOGIC := '0';
+    signal RegWriteE : STD_LOGIC := '0';
+    signal MemtoRegE : STD_LOGIC := '0';
+    signal MemWriteE : STD_LOGIC := '0';
+    signal ALUControlE : STD_LOGIC_VECTOR (2 downto 0) := (others => '0');
+    signal ALUSrcE : STD_LOGIC := '0';
+    signal RegDstE : STD_LOGIC := '0';
+    signal RD1E : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal RD2E : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal RD3E : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal RsE : STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+    signal RtE : STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+    signal RdE : STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+    signal SignImmE : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal CLR : STD_LOGIC := '0';
+     --nEN : in STD_LOGIC;
+    signal forwardAE : std_logic_vector(1 downto 0) := (others => '0');
+    signal forwardBE : std_logic_vector(1 downto 0) := (others => '0');
+    signal forwardCE : std_logic_vector(1 downto 0) := (others => '0');
+    signal ResultW : std_logic_vector(31 downto 0) := (others => '0');
+     --WriteRegE : inout std_logic_vector(4 downto 0);
+    signal RegWriteM : STD_LOGIC := '0';
+    signal MemtoRegM : STD_LOGIC := '0';
+    signal MemWriteM : STD_LOGIC := '0';
+    signal ALUOutM : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal WriteDataM : STD_LOGIC_VECTOR (31 downto 0) := (others => '0');
+    signal WriteRegM : STD_LOGIC_VECTOR (4 downto 0) := (others => '0');
+    
 begin
 
+    uut: execute_stage port map (clk, RegWriteE, MemtoRegE, MemWriteE, ALUControlE, ALUSrcE, RegDstE, RD1E, RD2E, RD3E, RsE, RtE, RdE, SignImmE,
+                                 CLR, forwardAE, forwardBE, forwardCE, ResultW, RegWriteM, MemtoRegM, MemWriteM, ALUOutM, WriteDataM, WriteRegM);
+
+    clk_process: process
+    begin 
+        clk <= not clk; wait for 5ns;
+    end process;
+    
+    stimulus_process: process 
+    begin
+        wait for 5ns;
+        RegWriteE <= '1'; MemtoRegE <= '0'; MemWriteE <= '0'; ALUControlE <= "000"; ALUSrcE <= '0'; RegDstE <= '1';
+        RD1E <= X"10000001"; RD2E <= X"11000000"; RD3E <= X"00110011";
+        RsE <= "00011"; RtE <= "00001"; RdE <= "00010"; SignImmE <= X"00000111"; CLR <= '0';
+        forwardAE <= "00"; forwardBE <= "00"; forwardCE <= "00";
+        ResultW <= X"11111111"; wait for 10ns;
+        forwardAE <= "10"; wait for 10ns;
+        forwardBE <= "01"; wait for 10ns;
+        forwardAE <= "00"; forwardBE <= "00"; wait for 10ns;
+        ALUSrcE <= '1'; RegDstE <= '0'; wait;
+        
+    end process;  
 
 end Behavioral;
